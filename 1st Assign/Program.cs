ed7b0337fc9ad7e5e629a90b5aefb45c221e02ec
Shyam Saran <ksshyamsaran189@gmail.com>
2024-09-20 11:34:00 +0530

    Add files via upload

---
+++ 1st Assign/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Find the area of the Square */
+
+using System;
+using System.ComponentModel;
+
+public class areaOfSquare
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the side of the Square : ");
+        double side = Convert.ToInt32(Console.ReadLine());
+
+        var area = side * side;
+
+        Console.WriteLine("The area of the square is : " + area);
+    }
+}
+
